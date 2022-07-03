758ee1b0abc735ab813fbb5f8ad44e637c7b595c
DUSARI96 <106464093+DUSARI96@users.noreply.github.com>
2022-07-03 07:56:54 +0530

    task001-030722756
    create  java file

---
+++ one.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+
+class one
+{
+  public static void main(string[] args)
+  {
+    system.out.println("welcome to java application");
+    system.out.println("welcome to java application");
+    system.out.println("welcome to java application");
+    system.out.println("welcome to java application");
+  }
+  
+}
